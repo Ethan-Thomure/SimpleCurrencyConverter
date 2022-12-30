780bff37078a1f9ab3319b8044e0d585c41b6020
Ethan Thomure <ethanthomure@icloud.com>
2022-12-30 09:42:36 -0600

    Stage 4

--- Simple Currency Converter/task/main.js
+++ Simple Currency Converter/task/main.js
@@ -5,26 +5,37 @@ console.log("Welcome to Currency Converter!");
 for (let i in conversions) {
     console.log(`1 USD equals ${conversions[i]} ${i}`);
 }
-console.log("What do you want to convert?");
-let convertFrom = input("From: ").toUpperCase();
-
-if (convertFrom in conversions) {
-    let convertTo = input("To: ").toUpperCase();
-
-    if (convertTo in conversions) {
-        const amount = input("Amount: ");
-
-        if (isNaN(amount)) {
-            console.log("The amount has to be a number");
-        } else if (amount < 1) {
-            console.log("The amount cannot be less than 1");
-        } else {
-            console.log(`Result: ${amount} ${convertFrom} equals ${
-                (amount * conversions[convertTo] / conversions[convertFrom]).toFixed(4)} ${convertTo}`);
-        }
+while (true) {
+    const whatToDo = input("What do you want to do?\n1-Convert currencies 2-Exit program\n");
+    if (whatToDo == 2) {
+        console.log("Have a nice day!");
+        break;
+    } else if (whatToDo != 1) {
+        console.log("Unknown input");
+        // continue;
     } else {
-        console.log("Unknown currency");
+        console.log("What do you want to convert?");
+        let convertFrom = input("From: ").toUpperCase();
+
+        if (convertFrom in conversions) {
+            let convertTo = input("To: ").toUpperCase();
+
+            if (convertTo in conversions) {
+                const amount = input("Amount: ");
+
+                if (isNaN(amount)) {
+                    console.log("The amount has to be a number");
+                } else if (amount < 1) {
+                    console.log("The amount cannot be less than 1");
+                } else {
+                    console.log(`Result: ${amount} ${convertFrom} equals ${
+                        (amount * conversions[convertTo] / conversions[convertFrom]).toFixed(4)} ${convertTo}`);
+                }
+            } else {
+                console.log("Unknown currency");
+            }
+        } else {
+            console.log("Unknown currency");
+        }
     }
-} else {
-    console.log("Unknown currency");
 }
